ffa40abbac3a532a7cd6d23caa84373e8ca5bce7
akambetov <almat_kambetov@mail.ru>
2023-11-17 00:45:28 +0600

    9 module - react(ls-79): style-prop type (React.CSSProperties)

--- src/9-react/src/App.tsx
+++ src/9-react/src/App.tsx
@@ -4,7 +4,7 @@ import TodoItem from './components/TodoItem';
 function App() {
   return (
     <div className="App">
-      <TodoItem id='1' title='Title 1' completed={false}/>
+      <TodoItem id='1' title='Title 1' completed={false} style={{color: 'yellow'}}/>
       <TodoItem id='2' title='Title 2' completed={true}/>
     </div>
   );
--- src/9-react/src/components/TodoItem.tsx
+++ src/9-react/src/components/TodoItem.tsx
@@ -5,10 +5,11 @@ interface ITodoItemProps {
   title: string
   completed: boolean
   children?: React.ReactNode // нужно явно узказывать тип children, если в компонент будет передаваться children
+  style?: React.CSSProperties
 }
 
-const TodoItem = ({ id, title, completed, children}: ITodoItemProps) => {
-  return <li>
+const TodoItem = ({ id, title, completed, children, style = {} }: ITodoItemProps) => {
+  return <li style={{color: 'red', backgroundColor: 'green', ...style}}>
     <input type="checkbox" checked={completed}/>
     <span>{title} - id {id}</span>
     <span>&times;</span>
