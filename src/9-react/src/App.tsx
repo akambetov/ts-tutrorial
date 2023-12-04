c9d0cf2176f1bb9fbd2b51063124cd2cd2ba8108
akambetov <almat_kambetov@mail.ru>
2023-12-04 21:04:55 +0600

    9 module - react(ls-80): state types (useState - App component)

--- src/9-react/src/App.tsx
+++ src/9-react/src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
+type ITodo = {
+  id: string,
+  title: string,
+  completed: string
+}
+
 function App() {
+  const [text, setText] = useState('')
+  // !!! при работе с массивами при укзании пустого массива в качестве inital, 
+  // то тип для стейта будуте массив never -  never[] 
+  // const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<string[]>([])
+  // !!! если надо засетить initial в качестве null
+  // то в дженерике указываем union из типа который нужен и null
+  //  в коде нужна будет проверка на null
+  // const [someS, setSomeS] = useState<string[] | null>(null)
+  // const [todo, setTodo] = useState<ITodo | null>(null)
+
   return (
     <div className="App">
       <TodoItem id='1' title='Title 1' completed={false} style={{color: 'yellow'}}/>
