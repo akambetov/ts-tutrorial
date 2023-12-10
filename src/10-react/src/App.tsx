fbd104174cd4e64841e2ea747422965ed8c1f0bd
akambetov <almat_kambetov@mail.ru>
2023-12-10 12:13:38 +0600

    10 module - react(ls-91): SearchWithRef (search form by useRef)

--- src/10-react/src/App.tsx
+++ src/10-react/src/App.tsx
@@ -1,10 +1,12 @@
 import { Container } from 'components/Container';
+import { SearchWithRef } from 'components/Search';
 import { TheHeader } from 'components/TheHeader';
 
 function App() {
   return (
     <Container>
       <TheHeader />
+      <SearchWithRef hasError onSubmit={() => {}} />
     </Container>
   );
 }
---
+++ src/10-react/src/components/Search/SearchWithRef.tsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react';
+import { Button } from 'components/Button';
+import { ReactComponent as SearchIcon } from 'assets/icon-search.svg';
+import styles from './Search.module.scss';
+
+interface SearchWithRefProps {
+  hasError: boolean;
+  onSubmit: (text: string) => void;
+}
+
+export const SearchWithRef = ({ hasError, onSubmit }: SearchWithRefProps) => {
+  const searchRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const text = searchRef.current?.value || '';
+
+    if (text) {
+      onSubmit(text);
+
+      if (searchRef.current) {
+        searchRef.current.value = '';
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <div className={styles.search}>
+        <label htmlFor="search" className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          ref={searchRef}
+          type="text"
+          className={styles.textField}
+          id="search"
+          name="username"
+          placeholder="Search Github username"
+        />
+        {hasError && <div className={styles.error}>No Result</div>}
+        <Button>Search</Button>
+      </div>
+    </form>
+  );
+};
