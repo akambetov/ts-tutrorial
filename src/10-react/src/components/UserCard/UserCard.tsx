0c8998a16a34828653d928aa3b5da97b3d78dda5
akambetov <almat_kambetov@mail.ru>
2023-12-10 16:04:53 +0600

    10 module - react(ls-94): UserCard display grid, UserTitle component

--- src/10-react/src/components/UserCard/UserCard.tsx
+++ src/10-react/src/components/UserCard/UserCard.tsx
@@ -1,11 +1,17 @@
 import { LocalGithubUser } from 'types';
 import { UserStat } from 'components/UserStat';
+import { UserTitle } from 'components/UserTitle';
 import styles from './UserCard.module.scss';
 
 interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img src={props.avatar} alt={props.login} className={styles.avatar} />
+    <UserTitle name={props.name} login={props.login} created={props.created} />
+    <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio'}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
